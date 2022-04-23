56f654dcbfa7341a38c41edf31410dad406f5a94
Fateme Zamanian <fatemehzamanian.1999@gmail.com>
2022-04-23 05:10:05 +0430

    torch1

---
+++ fashionMnistTorch/model.py
@@ -0,0 +1,23 @@
+import torch
+
+class MyModel(torch.nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.fc1=torch.nn.Linear(784,256)
+    self.fc2=torch.nn.Linear(256,64)
+    self.fc3=torch.nn.Linear(64,32)
+    self.fc4=torch.nn.Linear(32,16)
+    self.fc5=torch.nn.Linear(16,10)
+  def forward(self,x):
+    x=x.reshape((x.shape[0],784))
+    x=self.fc1(x)
+    x=torch.relu(x)
+    x=self.fc2(x)
+    x=torch.relu(x)
+    x=self.fc3(x)
+    x=torch.relu(x)
+    x=self.fc4(x)
+    x=torch.relu(x)
+    x=self.fc5(x)
+    x=torch.relu(x)
+    return x
---
+++ fashionMnistTorch/train.py
@@ -0,0 +1,57 @@
+import argparse
+import wandb
+import torch
+import torchvision
+
+from model import *
+from test import *
+
+wandb.init(project="fashionMnist")
+
+device=torch.device("cpu")
+model=MyModel()
+model=model.to(device)
+model.train(True)
+
+#hyper parameter
+batch_size=64
+epochs=20
+lr=0.01
+
+transform=torchvision.transforms.Compose([
+                                torchvision.transforms.ToTensor(),
+                                torchvision.transforms.Normalize((0),(1)),
+])
+
+dataset=torchvision.datasets.FashionMNIST("./dataset",train=True,transform=transform,download=True)
+train_data_loader=torch.utils.data.DataLoader(dataset,batch_size=batch_size,shuffle=True)
+
+optimizer=torch.optim.Adam(model.parameters(),lr=lr)
+loss_function=torch.nn.CrossEntropyLoss()
+
+for epoch in range(epochs):
+  train_loss=0.0
+  train_acc=0.0
+  for images,labels in train_data_loader:
+    images=images.to(device)
+    labels=labels.to(device)
+
+    optimizer.zero_grad()
+    pred=model(images)
+    loss=loss_function(pred,labels)
+    loss.backward()
+    optimizer.step()
+    train_loss+=loss
+    train_acc+=calc_acc(pred,labels)
+  
+  total_loss=train_loss/len(train_data_loader)
+  total_acc=train_acc/len(train_data_loader)
+  print(f"epochs: {epoch} , loss: {total_loss}, acc: {total_acc}")
+  wandb.log({'epochs': epoch+1,
+        'loss': total_loss,
+        'accuracy':total_acc
+        })
+
+torch.save(model.state_dict(),"fashion.pth")
+
+
